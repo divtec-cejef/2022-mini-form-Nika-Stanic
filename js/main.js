b48b8d907d803813d2d8e0b6f1fde0de82554808
Nika-Stanic <nika.stanic11@gmail.com>
2022-09-13 11:01:25 +0200

    creation boutons et label

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 /**
- * @author Steve Fallet
- * @since 2022.09.06
+ * @author Nika
+ * @since 2022.09.13
  */
 
 'use strict';
 
-console.log('Test liaison JS');
+//recuperer les elements du formulaire
+const formulaire = document.querySelector('form');
+
+//ecouter le formulaire
+formulaire.addEventListener('submit', function (event){
+    //annuler l'envoi du formulaire
+    event.preventDefault();
+    alert('Envoyé !');
+});
+
+//recuperer les elements liste deroulante
+const listeBackground = document.getElementById('background');
+
+//ecouter le changement de la valeur de la liste deroulante
+listeBackground.addEventListener('change', function(){
+    const image = `./img/backgounds/${listeBackground.value}`;
+    //changer l'image en fonction du choix choisi
+    document.body.style.backgroundImage = `url(image)`
+});
+
+
